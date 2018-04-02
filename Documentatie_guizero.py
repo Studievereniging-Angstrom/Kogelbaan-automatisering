d7522a5d599e986ffdf0468656b90f08b942805b
vanderEnd <35309393+vanderEnd@users.noreply.github.com>
2018-04-02 14:28:09 +0200

    Documentatie guizero
    In dit python bestand is een kleine documentatie van het pakket guizero opgenomen. Guizero is een pakket voor python dat gemaakt is om op een simpele manier een gui te maken.

--- Documentatie_guizero.py
+++ Documentatie_guizero.py
@@ -1,31 +1,38 @@
-'''in this progam the package guizero is being used to install this use pip install 
-   in the anaconda prompt. an internet connection is necessary'''
+#In de onderstaande code wordt Guizero gebruikt om een simpele gui te maken dit bestand dient als documentatie
 from guizero import App, Text, TextBox, PushButton, Slider, Picture, Combo, CheckBox, ButtonGroup
+#functies worden gedefinieerd die aangeroepen kunnen worden door buttons en sliders etc.  
 
 def Text_function():
         welcome_message.set(my_name.get())
-
+      
 def change_text_size(slider_value):
     welcome_message.font_size(slider_value)
 
-'First app is initiated'
+def choice():     
+    print( List_1.get() )
+    print( Checkbox_1.get_value() )
+    print( ButtonGroup_1.get() )
+
+#dit programma bestaat uit twee applicaties. een van de applicaties wordt hier geinitialiseerd 
 app = App(title="Hello world")
 
+#de app beste uit een text een textbox een pushbutton en een slider. 
+#deze objecten worden hier geinitialiseerd
 welcome_message = Text(app, text="Text with: size/font/color", size=10, font="Times new roman", color="red")
 my_name = TextBox(app, width=30)
 update_text = PushButton(app, command=Text_function, text="PushButton 1")
 text_size = Slider(app, command=change_text_size, start=10, end=80)
 
+#om de gui ook werkelijk te laten zien wordt deze hier aangeroepen
 app.display()
 #%%
-'Second app is defined'
-def choice():     
-    print( List_1.get() )
-    print( Checkbox_1.get_value() )
-    print( ButtonGroup_1.get() )
-'second app is initiated'
+
+#de 2e applicatie wordt gestart. 
 app2 = App(title = 'Second GUI', width=300 , height=200, layout='grid')
 
+#deze applicatie bestaat in tegenstelling tot de vorige applicatie uit een grid 
+#op deze grid worden verschillende onderdelen geplaatst zoals: 
+#een dropdown menu (Combo), een text, een checkbox, een buttongroep, nog een text, en normale button. 
 'A list/checkbox/buttongroup are defined in a grid'
 List_1 = Combo(app2, options=['List','First_item'], grid = [1,0],align='left')
 List_1_text = Text(app2, text="List:", grid=[0,0], align="left")
@@ -34,5 +41,6 @@ ButtonGroup_1 = ButtonGroup(app2,options=[['Front', 'Front'],['Middle', 'Middle'
 row_choice_text = Text(app2, text="Which row", grid=[0,3], align="left")
 show_choices = PushButton(app2, command=choice, text="Show choices", grid=[0,4], align='left')
 
+#om de gui ook werkelijk te laten zien wordt deze hier aangeroepen
 app2.display()
 
